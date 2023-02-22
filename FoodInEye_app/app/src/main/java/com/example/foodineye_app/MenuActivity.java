b949421fd499a949e22850d9d2918c3296dc3c32
suyeon801 <suyeon801@duksung.ac.kr>
2023-02-22 23:37:36 +0900

    connect Menu MenuDetail

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuActivity.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuActivity.java
@@ -2,7 +2,10 @@ package com.example.foodineye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button s1m1Btn = (Button) findViewById(R.id.s1m1Btn);
+        s1m1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s1m1Intent = new Intent(getApplicationContext(), MenuDetailActivity.class);
+                startActivity(s1m1Intent);
+            }
+        });
     }
 }
