12ddda34a74dbe2d2b4773b818a3b85755929ee1
suyeon801 <suyeon801@duksung.ac.kr>
2023-03-23 23:54:36 +0900

    activity_menu(진행중)

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuActivity.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodineye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +12,17 @@ import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    RecyclerView menurecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //menuRecyclerview
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        menurecyclerView.setLayoutManager(gridLayoutManager);
+
 
         Button s1m1Btn = (Button) findViewById(R.id.smBtn);
         s1m1Btn.setOnClickListener(new View.OnClickListener() {
