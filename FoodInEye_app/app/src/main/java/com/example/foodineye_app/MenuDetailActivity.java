776588042aed212558658c7afc545fef9648c5b4
suyeon801 <suyeon801@duksung.ac.kr>
2023-04-12 21:40:45 +0900

    add alert_dialog

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuDetailActivity.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/MenuDetailActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +16,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,7 @@ public class MenuDetailActivity extends AppCompatActivity {
     TextView menu_price;
 
     String m_Id, s_Id;
+    String m_name;
     List<Menus> menuInfo = new ArrayList<>();
 
     @Override
@@ -56,6 +60,7 @@ public class MenuDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         IntentToDetail intentToDetail = (IntentToDetail) intent.getSerializableExtra("intentToDetail");
 
+        m_name = intentToDetail.food.getM_name();
         s_Id = intentToDetail.getS_id();
         m_Id = intentToDetail.getM_id();
         Log.d("intentToDetail", "intentToDetail_sid" + s_Id);
@@ -85,27 +90,67 @@ public class MenuDetailActivity extends AppCompatActivity {
 
     }
     public void showDialog(){
-        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(MenuDetailActivity.this)
-                .setTitle("Order")
-                .setMessage("Order")
-                .setNegativeButton("더 담으러 가기", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-                        intent.putExtra("intent_SId", s_Id);
-                        intent.putExtra("intent_mId", m_Id);
-                        Log.d("Intent_id", "Intent_id: " + s_Id);
-                        startActivity(intent);
-                    }
-                })
-                .setPositiveButton("장바구니 담으러가기", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(getApplicationContext(), ShoppingCartActivity.class);
-                        startActivity(intent);
-                    }
-                });
-        AlertDialog msgDlg = msgBuilder.create();
-        msgDlg.show();
+
+        LayoutInflater layoutInflater = LayoutInflater.from(MenuDetailActivity.this);
+        View view = layoutInflater.inflate(R.layout.alert_dialog_custom, null);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MenuDetailActivity.this, R.style.CustomAlertDialog)
+                .setView(view)
+                .create();
+
+        TextView menuName = view.findViewById(R.id.alert_menuName);
+        TextView toMenu = view.findViewById(R.id.alert_toMenu);
+        TextView toCart = view.findViewById(R.id.alert_toCart);
+
+        menuName.setText(m_name);
+        toMenu.setText("더 담으러 가기");
+        toCart.setText("장바구니 가기");
+
+        toMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                intent.putExtra("intent_SId", s_Id);
+                intent.putExtra("intent_mId", m_Id);
+                Log.d("Intent_id", "Intent_id: " + s_Id);
+                startActivity(intent);
+            }
+        });
+
+        toCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShoppingCartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.show();
+
+
+
+
+//        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(MenuDetailActivity.this)
+//                .setTitle("Order")
+//                .setMessage("Order")
+//                .setNegativeButton("더 담으러 가기", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+//                        intent.putExtra("intent_SId", s_Id);
+//                        intent.putExtra("intent_mId", m_Id);
+//                        Log.d("Intent_id", "Intent_id: " + s_Id);
+//                        startActivity(intent);
+//                    }
+//                })
+//                .setPositiveButton("장바구니 담으러가기", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Intent intent = new Intent(getApplicationContext(), ShoppingCartActivity.class);
+//                        startActivity(intent);
+//                    }
+//                });
+//        AlertDialog msgDlg = msgBuilder.create();
+//        msgDlg.show();
     }
 }
