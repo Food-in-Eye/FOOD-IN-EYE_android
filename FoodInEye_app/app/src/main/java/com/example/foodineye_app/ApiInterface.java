2303ae143423ec796de3b24f905b894946ca8e61
suyeon801 <suyeon801@duksung.ac.kr>
2023-05-04 02:27:06 +0900

    cerate post interface

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/ApiInterface.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/ApiInterface.java
@@ -3,7 +3,9 @@ package com.example.foodineye_app;
 import android.view.Menu;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -14,4 +16,7 @@ public interface ApiInterface {
 
     @GET("api/v2/menus/menu/foods")
     Call<MenuItem> getMenusData(@Query("id") String m_id);
+
+    @POST("api/v2/orders/order")
+    Call<PostOrder> createOrder(@Body PostOrder postOrder);
 }
