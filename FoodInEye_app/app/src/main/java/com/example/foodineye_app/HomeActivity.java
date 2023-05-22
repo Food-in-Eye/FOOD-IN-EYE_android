4ea830bda537da4cfbdfa8af80b96b47e82167ff
suyeon801 <suyeon801@duksung.ac.kr>
2023-05-22 22:54:08 +0900

    add home_mypage, order, orderdetail

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/HomeActivity.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/HomeActivity.java
@@ -2,7 +2,11 @@ package com.example.foodineye_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        LinearLayout mypageBtn = (LinearLayout) findViewById(R.id.home_mypage);
+        mypageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(getApplicationContext(), MypageActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        LinearLayout orderdetailBtn = (LinearLayout) findViewById(R.id.home_orderdetail);
+        orderdetailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(getApplicationContext(), OrderDetailActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        LinearLayout orderBtn = (LinearLayout) findViewById(R.id.home_order);
+        orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(getApplicationContext(), CategoryActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
