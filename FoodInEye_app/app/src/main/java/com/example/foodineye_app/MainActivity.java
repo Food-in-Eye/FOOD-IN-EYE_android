adb237223220cf250ab98eb0aec95f50cea60382
suyeon801 <suyeon801@duksung.ac.kr>
2023-06-01 23:41:26 +0900

    add seeso SDK

--- FoodInEye_app/app/src/main/java/com/example/foodineye_app/MainActivity.java
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+
+        Log.d("display", "screenWidth: "+screenWidth);
+        Log.d("display", "screenHeight: "+screenHeight);
 
         Button startBtn = (Button) findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
