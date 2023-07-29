21b240bc6b4e087658567309547f41231768d46d
suyeon801 <suyeon801@duksung.ac.kr>
2023-07-30 01:42:26 +0900

    create order_history.xml

---
+++ FoodInEye_app/app/src/main/java/com/example/foodineye_app/activity/OrderHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodineye_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodineye_app.R;
+
+public class OrderHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_history);
+    }
+}
